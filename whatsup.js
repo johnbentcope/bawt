ce03f8c26a4fe6fe190ce937682db1d6126212c4
John Cope <john@riflepaperco.com>
2015-06-03 12:44:21 -0400

    yup

--- whatsup.js
+++ whatsup.js
@@ -145,7 +145,7 @@ function send (payload, callback) {
 		method: 'POST',
 		body: JSON.stringify(payload)
 	}, function (error, response, body) {
-		if(error || payload.indexOf("Looks like a slow day") >= 0 ) {
+		if(error) {
 			return callback(error)
 		}
 
